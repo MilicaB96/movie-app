076d6e5223473dcab05a762144a677a8290a4ec3
Milica Baštrica <milicabastrica@Milicas-MacBook-Pro.local>
2022-05-27 10:07:49 +0200

    spelling error fix

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,7 +3,7 @@ import Navbar from "../navbar/Navbar";
 import PrivateRoute from "../../shared/routes/routes/PrivateRoute";
 import ROUTES from "../../shared/routes/routes";
 import CreateMoviePage from "../movie/createmovie/CreateMoviePage";
-import MoviePage from "../movie/listmovie/MoivePage";
+import MoviePage from "../movie/listmovie/MoviePage";
 
 function HomePage() {
   return (
